aa02b324e1833a91b3d74cb424b4807dbf6a19c9
JeanPaulRF <64498696+JeanPaulRF@users.noreply.github.com>
2023-03-13 20:05:27 -0600

    struct proceso

--- proceso.c
+++ proceso.c
@@ -6,4 +6,10 @@ struct Proceso
     int pid;
     int burst;
     int prioridad;
+    int tiempoLlegada;
+    int tiempoSalida;
+    int tat;
+    int wt;
+    int estado; // 0: listo, 1: ejecutando, 2: terminado
+    int burstRestante;
 };
