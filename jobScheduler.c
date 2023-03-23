2f77fe9d2421e0343f59bff031290e8244c1106f
DavidEspinozaNemo <david1998es16@gmail.com>
2023-03-23 13:05:09 -0600

    sigo con los mismos errores

--- jobScheduler.c
+++ jobScheduler.c
@@ -20,7 +20,7 @@ typedef struct
 } JobScheduler;
 
 // crear la estructura
-struct JobScheduler *new_JobScheduler(){
+struct* JobScheduler new_JobScheduler(){
 	JobScheduler *new_JS;
 	new_JS->cont_PID = 1; // 0 a 4294967295
 	new_JS->cant_JOB = 50;
@@ -29,9 +29,17 @@ struct JobScheduler *new_JobScheduler(){
 	return new_JS;
 }
 
-/* Reallocating memory */
-//   READY = (Proceso *) realloc(READY, 70);
+// Funcion para agrandar la cola READY
+/*
+void agrandar_cola_READY(struct JobScheduler *JS){
+	// el 70 seria el total ya aumentada
+	// Reallocating memory
+	JS->READY = (Proceso *) realloc(READY, 70); 
+}
+*/
 
+// insertar un nuevo proceso
+/*
 void insert_job(struct JobScheduler *JS, int v_burst, int v_prioridad){
 	// crea el proceso
 	Proceso newJOB;
@@ -39,26 +47,62 @@ void insert_job(struct JobScheduler *JS, int v_burst, int v_prioridad){
 	newJOB.burst = v_burst;
 	newJOB.prioridad = v_prioridad;
 	
+	if( JS->index == JS->cant_JOB ) agrandar_cola_READY(JS);
+	
 	JS->READY[JS->index] = newJOB;
 	JS->index += 1; //si se llena funcion para agrandar READY
 	JS->cont_PID += 1;
-};
+};*/
 
 // Funcion para enviar mensaje al cliente con el PID
 
 // Funcion para recibir un Proceso del CPU Scheduler
+/*
+void reinsert_job(struct JobScheduler *JS, Proceso *job){
+	if( JS->index == JS->cant_JOB ) agrandar_cola_READY();
+	else{
+		JS->READY[JS->index] = job;
+		JS->index += 1;
+	}	
+}
+*/
+
+// Funcion para reagustar
+/*
+void reajuste_READY( struct JobScheduler *JS, v_indice int){
+	int i = v_indice;
+	
+	while( i < JS->cant_JOB){
+		JS->READY[i] = JS->READY[i+1];
+	}
+	JS->index -= 1;
+}
+*/ 
 
 // Funcion para sacar un Proceso
-
-// Funcion para agrandar la cola READY
+/*
+Proceso get_job( struct JobScheduler *JS, v_pid int){
+	int i = 0;
+	Proceso newJOB;
+	
+	while( i < JS->cant_JOB){
+		if ( JS->READY[i].pid == v_pid ){
+			newJOB = JS->READY[i].pid;
+			reajuste_READY(JS, i);
+			return newJOB;
+		}
+		else i++;
+	}
+	return newJOB; //le caeria vacio.
+}
+*/
 
 int main(){
-	struct JobScheduler *JS;
+	struct JobScheduler *JS = (struct* JobScheduler) new_JobScheduler();
 	int a,b;
-	JS = new_JobScheduler();
-	insert_job(JS, 3, 4);
+	//insert_job(JS, 3, 4);
 	
-	printf("Inserted: %d\n", (JS->READY[0]).pid );
+	//printf("Inserted: %d\n", (JS->READY[0]).pid );
 	
 	free(JS);
 	return 0;
