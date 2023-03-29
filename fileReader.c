bb1e007736a7d7ffef2f86fcabd5ce90425b4d86
Taylor S. Hernández <taylor20sep.hc@gmail.com>
2023-03-28 18:39:36 -0600

    File Reader listo

--- fileReader.c
+++ fileReader.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-// Definición de la estructura de un nodo de la lista
+// Estructura para almacenar los valores
 typedef struct nodo {
     int valor;
     struct nodo* siguiente;
 } Nodo;
 
-// Función para agregar un valor a la lista
+// Con esta función agrego un valor
 void agregar(Nodo** cabeza, int valor) {
     Nodo* nuevo_nodo = (Nodo*) malloc(sizeof(Nodo));
     nuevo_nodo->valor = valor;
@@ -24,56 +25,44 @@ void agregar(Nodo** cabeza, int valor) {
 }
 
 int main() {
-    // Abrir el archivo de texto
+    // Abrir archivo 
     FILE* archivo = fopen("archivo.txt", "r");
     if (archivo == NULL) {
         printf("No se pudo abrir el archivo\n");
         return 1;
     }
 
-    // Inicializar las listas para almacenar los datos
     Nodo* lista_burst = NULL;
     Nodo* lista_prioridad = NULL;
 
-    // Leer cada línea del archivo
-    int burst, prioridad;
-    while (fscanf(archivo, "%d %d", &burst, &prioridad) == 2) {
-        // Agregar los datos a las respectivas listas
-        agregar(&lista_burst, burst);
-        agregar(&lista_prioridad, prioridad);
+    char linea[100];
+    fgets(linea, 100, archivo);
+    char* burst = "Burst";
+    char* prioridad = "Prioridad";
+
+    // Agregar los datos
+    int valor_burst, valor_prioridad;
+    while (fscanf(archivo, "%d\t%d", &valor_burst, &valor_prioridad) == 2) {
+        agregar(&lista_burst, valor_burst);
+        agregar(&lista_prioridad, valor_prioridad);
     }
 
-    // Cerrar el archivo
     fclose(archivo);
 
-    // Imprimir los datos de las listas
-    printf("Lista Burst:\n");
+    // Imprimir las listas
+    printf("Lista %s:\n", burst);
     Nodo* nodo_burst = lista_burst;
     while (nodo_burst != NULL) {
         printf("%d\n", nodo_burst->valor);
         nodo_burst = nodo_burst->siguiente;
     }
-    printf("\nLista Prioridad:\n");
+    printf("\nLista %s:\n", prioridad);
     Nodo* nodo_prioridad = lista_prioridad;
     while (nodo_prioridad != NULL) {
         printf("%d\n", nodo_prioridad->valor);
         nodo_prioridad = nodo_prioridad->siguiente;
     }
 
-    // Liberar la memoria de las listas
-    Nodo* nodo_actual = lista_burst;
-    Nodo* nodo_siguiente;
-    while (nodo_actual != NULL) {
-        nodo_siguiente = nodo_actual->siguiente;
-        free(nodo_actual);
-        nodo_actual = nodo_siguiente;
-    }
-    nodo_actual = lista_prioridad;
-    while (nodo_actual != NULL) {
-        nodo_siguiente = nodo_actual->siguiente;
-        free(nodo_actual);
-        nodo_actual = nodo_siguiente;
-    }
 
     return 0;
 }
