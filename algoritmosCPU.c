925af7cbefb6cb4b72ab315737b1eaebf268e865
RonaldCF <ronaldcordero32@gmail.com>
2023-03-30 19:08:03 -0600

    algoritmos con variables globales

--- algoritmosCPU.c
+++ algoritmosCPU.c
@@ -3,64 +3,59 @@
 #include <limits.h>
 #include "jobScheduler.c"
 
-node_js *EXIT;
-
 void algoritmoFifo();
-void algoritmoSjf(node_js **headReady, node_js **headExit);
-int getPidShortestBurst(node_js **headReady); // pid del proceso con menos burst
-void algoritmoHpf(node_js **headReady, node_js **headExit);
-int getPidShortestPrioridad(node_js **headReady);
-void roundRobin(node_js **headReady, node_js **headExit, int quantum);
+void algoritmoSjf();
+int getPidShortestBurst(); // pid del proceso con menos burst
+void algoritmoHpf();
+int getPidShortestPrioridad();
+void roundRobin(int quantum);
 
 // Algoritmo FIFO
 void algoritmoFifo()
 { 
 // pasar el ready
-  Proceso procesoActual;
+  node_js *nodeActual;
   node_js *tmp = READY;
   int vpid = 0; // probar asignacion a NULL
   
-  vpid = tmp->data.pid;
-  procesoActual = get_proceso(READY, vpid);
+  vpid = tmp->data->pid;
+  nodeActual = find_nodeJS(&READY, vpid); //deberia ser el head
 
-   //EXIT = recibe_job(headExit, procesoActual);
-  if(procesoActual.pid != 0){
-  	node_js *nodoTmp = malloc(sizeof(node_js));
-  	nodoTmp->data = procesoActual;
-  	nodoTmp->NEXT = NULL;
-  	insert_at_head(&EXIT, nodoTmp);
+  if(nodeActual->data->pid != 0){
+    delete_nodeJS(&READY, nodeActual);
+  	insert_at_start(&EXIT, nodeActual);
   }
 
 }
 
 // Algoritmo SJF
-void algoritmoSjf(node_js **headReady, node_js **headExit)
+void algoritmoSjf()
 {
-  int indexShortest;
-  int repeticiones = 0;
   int vpid = 0; // probar asignacion a NULL
-  node_js *temporary = headReady;
-  Proceso procesoActual;
-  node_js *tmp = headReady;
-  vpid = getPidShortestBurst(&headReady);
-  procesoActual = get_proceso(headReady, vpid);
+  node_js *nodeActual;
+  vpid = getPidShortestBurst();
+  nodeActual = find_nodeJS(&READY, vpid);  
   
-  if(procesoActual.pid != 0) headExit = recibe_job(headExit, procesoActual);
+  if(nodeActual->data->pid != 0) 
+  {
+    delete_nodeJS(&READY, nodeActual);
+    insert_at_start(&EXIT, nodeActual);
+  } 
   // else, pos no hace nada
 }
 
-int getPidShortestBurst(node_js **headReady)
-{ // los procesos que ya pasaron se setean en burst = -1
-  node_js *masPequenio = headReady;
+int getPidShortestBurst()
+{ 
+  node_js *masPequenio = READY; // puede que deba corregirse READY a &READY
   int burstTmp = INT_MAX;
   int indexShortest = INT_MAX;
 
   while (masPequenio != NULL)
   {
-    if (masPequenio->data.burst < burstTmp)
+    if (masPequenio->data->burst < burstTmp)
     { // si burstRestante = 0, ya fue procesado
-      indexShortest = masPequenio->data.pid;
-      burstTmp = masPequenio->data.burst;
+      indexShortest = masPequenio->data->pid;
+      burstTmp = masPequenio->data->burst;
     }
     masPequenio = masPequenio->NEXT;
   }
@@ -68,33 +63,37 @@ int getPidShortestBurst(node_js **headReady)
 }
 
 // Algoritmo HPF
-void algoritmoHpf(node_js **headReady, node_js **headExit)
+void algoritmoHpf()
 {
-  int indexShortest;
-  int repeticiones = 0;
+  //int indexShortest;
+  //int repeticiones = 0;
   int vpid = 0; // probar asignacion a NULL
-  node_js *temporary = headReady;
-  Proceso procesoActual;
-  node_js *tmp = headReady;
-  vpid = getPidShortestPrioridad(&headReady);
-  procesoActual = get_proceso(headReady, vpid);
+  //node_js *temporary = headReady;
+  node_js *nodeActual;
+  //node_js *tmp = headReady;
+  vpid = getPidShortestPrioridad();
+  nodeActual = find_nodeJS(&READY, vpid);
   
-  if(procesoActual.pid != 0) headExit = recibe_job(headExit, procesoActual);
+  if(nodeActual->data->pid != 0)
+  {
+    delete_nodeJS(&READY, nodeActual);
+    insert_at_start(&EXIT, nodeActual);
+  }
   // else, pos no hace nada
 }
 
-int getPidShortestPrioridad(node_js **headReady)
+int getPidShortestPrioridad()
 {
-  node_js *masPequenio = headReady;
+  node_js *masPequenio = READY;   // puede que deba corregirse READY a &READY
   int prioridadTmp = INT_MAX;
   int indexShortest = INT_MAX;
 
   while (masPequenio != NULL)
   {
-    if (masPequenio->data.burst < prioridadTmp)
+    if (masPequenio->data->burst < prioridadTmp)
     { // si burstRestante = 0, ya fue procesado
-      indexShortest = masPequenio->data.pid;
-      prioridadTmp = masPequenio->data.prioridad;
+      indexShortest = masPequenio->data->pid;
+      prioridadTmp = masPequenio->data->prioridad;
     }
     masPequenio = masPequenio->NEXT;
   }
@@ -102,14 +101,18 @@ int getPidShortestPrioridad(node_js **headReady)
 }
 
 // Algoritmo Round Robin
-void roundRobin(node_js **headReady, node_js **headExit, int quantum)
+void roundRobin(int quantum)
 {
-  Proceso procesoActual;
-  node_js *tmp = headReady;
+  node_js *nodeActual;
+  node_js *tmp = READY;  // puede que deba corregirse READY a &READY
   int vpid = 0; // probar asignacion a NULL
-  vpid = tmp->data.pid;
-  procesoActual = get_proceso(headReady, vpid);
+  vpid = tmp->data->pid;
+  nodeActual = find_nodeJS(&READY, vpid);  //get_proceso(headReady, vpid);
   
-  if(procesoActual.pid != 0) headExit = recibe_job(headExit, procesoActual);
+  if(nodeActual->data->pid != 0)
+  {
+    delete_nodeJS(&READY, nodeActual);
+    insert_at_start(&EXIT, nodeActual);
+  }
   // else, pos no hace nada
 }
