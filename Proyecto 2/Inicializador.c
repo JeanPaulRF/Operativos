b7e7e5e8838c083b834dffc8be3c3eebf7983d4d
JeanPaulRF <64498696+JeanPaulRF@users.noreply.github.com>
2023-05-12 21:59:17 -0600

    estructura de control

--- Proyecto 2/Inicializador.c
+++ Proyecto 2/Inicializador.c
@@ -9,7 +9,25 @@
 #define SEM_KEY 1234
 #define SEM_KEY2 5678
 #define SIZE_LINEA 60
+#define SIZE_CONTROL 100
 #define N_SEMAPHORES 2
+#define MAX_PROCESOS 100
+
+typedef struct
+{
+    int pid_logico;
+    int pid_fisico;
+    int tipo;   // 0 = reader, 1 = writer, 2 = reader egoista
+    int estado; // 0 = bloqueado, 1 = dormido, 2 = ejecutando
+} Proceso;
+
+// estructura para el control de procesos
+typedef struct
+{
+    Proceso[MAX_PROCESOS] * procesos;
+    int count;
+    int sem_id;
+} Control;
 
 // función auxiliar para inicializar un semáforo
 void init_sem(int sem_id, int val)
@@ -34,6 +52,7 @@ int main(int argc, char *argv[])
     int shm_id;
     int lineas;
     int *mem;
+    *Control control;
 
     printf("Ingrese el numero de lineas que desea: ");
     scanf("%d", &lineas);
@@ -43,10 +62,17 @@ int main(int argc, char *argv[])
 
     // crear los semáforos
 
+    // semaforo de control
+    control->sem_id = semget(SEM_KEY, 1, IPC_CREAT | 0666);
+    if (control->sem_id == -1)
+    {
+        error("Error en semget");
+    }
+
     // semáforos para la memoria compartida
     for (i = 0; i < lineas; i++)
     {
-        sem_ids[i] = semget(SEM_KEY + i, 1, IPC_CREAT | 0666);
+        sem_ids[i] = semget(SEM_KEY + 1 + i, 1, IPC_CREAT | 0666);
         if (sem_ids[i] == -1)
         {
             error("Error en semget");
@@ -56,7 +82,7 @@ int main(int argc, char *argv[])
     // semaforos para readers
     for (i = 0; i < lineas; i++)
     {
-        sem_reader_ids[i] = semget(SEM_KEY2 + i, 1, IPC_CREAT | 0666);
+        sem_reader_ids[i] = semget(SEM_KEY2 + 1 + i, 1, IPC_CREAT | 0666);
         if (sem_reader_ids[i] == -1)
         {
             error("Error en semget");
@@ -64,6 +90,9 @@ int main(int argc, char *argv[])
     }
 
     // inicializar los semáforos
+
+    // semaforo de control
+    init_sem(control->sem_id, 1);
 
     // semáforos para la memoria compartida
     for (i = 0; i < lineas; i++)
@@ -92,7 +121,8 @@ int main(int argc, char *argv[])
     }
 
     printf("Se ha creado la memoria compartida con ID %d de %d bytes dividida en %d lineas.\n", shm_id, SIZE_LINEA * lineas, lineas);
-    printf("Se han creado %d semaforos.\n", N_SEMAPHORES * lineas);
+    printf("Se han creado %d semaforos para la memoria.\n", N_SEMAPHORES * lineas);
+    printf("Se ha creado un semaforo de control.\n");
 
     return 0;
 }
