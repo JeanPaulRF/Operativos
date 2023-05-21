70ee2f9fc44f78bc2caae2791c05eff6aa7cc7aa
ronald <ronaldcordero32@gmail.com>
2023-05-21 17:39:32 -0600

    reader threads

--- Proyecto2/Reader.c
+++ Proyecto2/Reader.c
@@ -1,65 +1,390 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <time.h>
+#include <pthread.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/sem.h>
-#include "Inicializador.c"
 
-#define SHM_KEY 9999
-#define SEM_KEY 1234
-#define SEM_KEY2 5678
+#include "Mensaje.h"
 
-/**
-void shmat(int shmid, void *shmaddr, int shmfi); 		// shmaddr se pone en 0 y el SO lo asigna
-int shmdt(void *shmaddr);						// shmaddr se pone en 0 y el SO lo asigna
+#define SHM_KEY 1234
+#define SEM_KEY_CONTROL 6666
+#define SEM_KEY_MEMORIA 7777
+#define MAX_PROCESOS 100
+#define SIZE_CONTROL (int)sizeof(Control) // tamaño de la estructura
 
+typedef struct
+{
+    int pid;
+    int tipo;   // 0 = reader, 1 = writer, 2 = reader egoista
+    int estado; // 0 = bloqueado, 1 = dormido, 2 = ejecutando
+} Proceso;
 
-int main(){
+// estructura para el control de procesos
+typedef struct
+{
+    Proceso procesos[MAX_PROCESOS];
+    int count;
+    int lineas;
+} Control;
+
+/* Parametros a mandar para el proceso */
+typedef struct
+{
+    int pid;
+    int tiempo_leer;
+    int tiempo_dormir;
+} parametrosLector;
+
+// parametros globales memoria compartida
+int shm_id;
+int semid_control;
+int semid_memoria;
+int lineas;
+Control *control;
+Mensaje *mensajes;
+
+// Función para realizar la operación wait (P) en el semáforo
+void sem_wait(int sem_id)
+{
+    struct sembuf sb;
+    sb.sem_num = 0; // Número del semáforo en el conjunto
+    sb.sem_op = -1; // Operación wait (P)
+    sb.sem_flg = 0; // Sin banderas adicionales
+
+    semop(sem_id, &sb, 1);
+}
+
+// Función para realizar la operación signal (V) en el semáforo
+void sem_signal(int sem_id)
+{
+    struct sembuf sb;
+    sb.sem_num = 0; // Número del semáforo en el conjunto
+    sb.sem_op = 1;  // Operación signal (V)
+    sb.sem_flg = 0; // Sin banderas adicionales
+
+    semop(sem_id, &sb, 1);
+}
+
+/* Leer el mensaje  */
+Mensaje read_message(int pid)
+{                     // recibe el id del proceso que lo crea
+    Mensaje MS = {0}; // Todos los miembros inicializan en 0.
+
+    time_t tiempoActual; // declaro variable
+    time(&tiempoActual); // obtengo el tiempo
+
+    struct tm *fechaHora = localtime(&tiempoActual);
+
+    MS.pid = pid;
+    MS.year = fechaHora->tm_year + 1900;
+    MS.month = fechaHora->tm_mon + 1;
+    MS.day = fechaHora->tm_mday;
+    MS.hour = fechaHora->tm_hour;
+    MS.minute = fechaHora->tm_min;
+    MS.second = fechaHora->tm_sec;
+    MS.mensaje = 1; // esta lleno
+
+    return MS;
+}
+
+/* Describe el comportamiento de todo proceso lector */
+void *preader(void *arg)
+{
+
+    parametrosLector *parametros = (parametrosLector *)arg;
+    int id = parametros->pid;
+    // printf("\n--> ID: %d\n", parametros->pid);
+
+    int estado = 1; // 1 esta escribiendo, 0 esta dormido
+    int pr_tiempo_leer = parametros->tiempo_leer;
+    int pr_tiempo_dormir = 0;
+
+    // Mensaje new_ms = create_message(id);	// se movio al for 
+
+    // ciclo while(1)
+    // si esta despierto busca una linea que contenga algo
+    // si esta dormido, simplemente sleep
+    sem_wait(semid_control);
+
+    // Manejar datos de control
+    control->procesos[id].estado = 2;
+
+    // signal semaforo control
+    sem_signal(semid_control);
+
     while (1)
     {
-        sem_wait(semB);
-        // Acceder a la memoria compartida para el tipo B
-        // Esperar a que no haya procesos de tipo A o C en la memoria compartida
+        // printf("\n\n pw_tiempo_escribir : %d \n", pw_tiempo_escribir);
+        // printf(" pw_tiempo_dormir : %d\n\n", pw_tiempo_dormir);
 
-        // shmat para acceder al segmento de memoria
-        // shmdt para salir de memoria al terminar
+        if (pr_tiempo_leer > 0)
+        {
+            // comportamiento cuando esta despierto
 
-        key_t key = ftok("memoria_compartida", SEM_KEY2 + 1);	// generar key unica (65 id asociado al file)
+            // actualizar estado
+            sem_wait(semid_control);
 
-        int shmid = shmget(key, SHM_SIZE, 0666);	// retorna id		( 1024 size de memoria , 0666 permiso para leer y escribir)
+            // Manejar datos de control
+            control->procesos[id].estado = 0;
 
-        char *str = (char*) shmat(shmid, (void*), 0 , 0);		// attach
+            // signal semaforo control
+            sem_signal(semid_control);
 
-        // ... //
-        printf("Datos en memoria:  %s\n", str);
-        shmdt(str);	// detach
+            // entra en memoria
+            void *memoria = shmat(shm_id, NULL, 0);
+            if (memoria == (void *)-1)
+            {
+                perror("shmat");
+                exit(1);
+            }
 
+            Mensaje *mensajes = (Mensaje *)memoria + SIZE_CONTROL;
 
-        sem_wait(semA);
-        sem_wait(semC);
-        sem_post(semA);
-        sem_post(semC);
-        // Realizar otras tareas
-        break;
-        sem_post(semB);
+            // wait semaforo memoria
+            sem_wait(semid_memoria);
+
+            // Extraer los datos de memoria
+            for (int i = 0; i < lineas; i++)
+            {
+                Mensaje mensaje = mensajes[i];
+
+                if (mensaje.mensaje == 1)
+                {
+                    printf("\n\n--> Proceso ID: %d\n", mensaje.pid);
+					printf("--> Fecha leídal: %d-%02d-%02d\n", mensaje.year, mensaje.month, mensaje.day);
+					printf("--> Hora leida: %02d:%02d:%02d\n", mensaje.hour, mensaje.minute, mensaje.second);
+                }
+            }
+
+            // signal semaforo memoria
+            sem_signal(semid_memoria);
+
+            if (shmdt(memoria) == -1)
+            {
+                perror("shmdt");
+                exit(1);
+            }
+			/*
+			printf("Print fuera del for");
+            printf("\n\n--> Proceso ID: %d\n", mensaje.pid);
+            printf("--> Fecha leídal: %d-%02d-%02d\n", mensaje.year, mensaje.month, mensaje.day);
+            printf("--> Hora leida: %02d:%02d:%02d\n", mensaje.hour, mensaje.minute, mensaje.second);*/
+            // sleep(5);
+            //  manda a avisar al control que esta despierto
+
+            // resto 1 tiempo por accion
+            pr_tiempo_leer = pr_tiempo_leer - 1;
+        }
+        else if (pr_tiempo_dormir > 0)
+        {
+            // comportamiento cuando esta dormido
+            printf("----> ZZzzZZ \n");
+            // sleep(5);
+            //  manda a avisar al control que esta dormido
+
+            // resto 1 tiempo por accion
+            pr_tiempo_dormir = pr_tiempo_dormir - 1;
+        }
+
+        // Para el cambio de estado, y rellenar el tiempo.
+        if (pr_tiempo_leer == 0 && estado == 1)
+        {
+            // si el tiempo de escribir se agoto y estoy en el estado de lectura
+            // relleno el tiempo de dormir
+            pr_tiempo_dormir = parametros->tiempo_dormir;
+            estado = 0; // lo paso al estado de Descanso
+
+            sem_wait(semid_control);
+
+            // Manejar datos de control
+            control->procesos[id].estado = 1;
+
+            // signal semaforo control
+            sem_signal(semid_control);
+        }
+        else if (pr_tiempo_dormir == 0 && estado == 0)
+        {
+            // si el tiempo de dormir se agoto y estoy en el estado de Descanso
+            // relleno el tiempo de leer
+            pr_tiempo_leer = parametros->tiempo_leer;
+            estado = 1; // lo paso al estado de lectura
+
+            sem_wait(semid_control);
+
+            // Manejar datos de control
+            control->procesos[id].estado = 2;
+
+            // signal semaforo control
+            sem_signal(semid_control);
+        }
+        sleep(1);
     }
+
+    // cuando finalize :
+    pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[])
+{
+    int NL, SL, SD;
+    /*
+      NL significa Numero de Lectores
+      SL siginifica Segundos de Lectura
+      SD significa Segundos de Dormir
+     */
+
+    // Mensaje ms1; // para pruubas
+    // ms1 = create_message(23987);
+
+    printf("-------------BIENVENIDO AL PROGRAMA LECTOR DEL SISTEMA-------------\n\n");
+
+    printf("Ingrese el numero de procesos lectores: ");
+    scanf("%d", &NL);
+
+    printf("Ingrese el numero segundos que leeran: ");
+    scanf("%d", &SL);
+
+    printf("Ingrese el numero segundos que dormiran ");
+    scanf("%d", &SD);
+
+    // id de los procesos
+    key_t key = ftok("memoria_compartida", 'R'); // usar la misma clave que en el otro programa
+    if (key == -1)
+    {
+        perror("ftok");
+        exit(1);
+    }
+
+    shm_id = shmget(key, 0, 0666);
+    if (shm_id == -1)
+    {
+        perror("shmget");
+        exit(1);
+    }
+
+    semid_control = semget(SEM_KEY_CONTROL, 1, 0666);
+    if (semid_control == -1)
+    {
+        perror("Error al acceder al semáforo control");
+        exit(1);
+    }
+
+    semid_memoria = semget(SEM_KEY_MEMORIA, 1, 0666);
+    if (semid_control == -1)
+    {
+        perror("Error al acceder al semáforo memoria");
+        exit(1);
+    }
+
+    // wait semaforo control
+    sem_wait(semid_control);
+
+    void *memoria = shmat(shm_id, NULL, 0);
+    if (memoria == (void *)-1)
+    {
+        perror("shmat");
+        exit(1);
+    }
+
+    Control *control = (struct Control *)memoria;
+
+    // Extraer las lineas de control
+    lineas = control->lineas;
+
+    // signal semaforo control
+    sem_signal(semid_control);
+
+    if (shmdt(memoria) == -1)
+    {
+        perror("shmdt");
+        exit(1);
+    }
+    // fin memoria compartida
+
+    /* // pruebas
+    printf(" Numero de procesos: %d\n Segundos de escritura: %d\n Segundos de descanso: %d\n", NE, SE, SD);
+    printf(" Fecha actual: %d-%02d-%02d\n", ms1.year, ms1.month, ms1.day);
+    printf(" Hora actual: %02d:%02d:%02d\n", ms1.hour, ms1.minute, ms1.second);
+    */
+    // %02d es una especificacion formato, para un numero con menos de dos digitos, que rellene un 0
+
+    /* Creacion de los threads */
+    pthread_t hilos[NL];
+    parametrosLector par_Reader[NL];
+
+    for (int i = 0; i < NL; i++)
+    {
+        par_Reader[i].tiempo_leer = SL;
+        par_Reader[i].tiempo_dormir = SD;
+
+        /* Por aqui deberia registrar los procesos al Control */
+
+        void *memoria = shmat(shm_id, NULL, 0);
+        if (memoria == (void *)-1)
+        {
+            perror("shmat");
+            exit(1);
+        }
+
+        control = (struct Control *)memoria;
+
+        // wait semaforo control
+        sem_wait(semid_control);
+
+        // Manejar datos de control
+        int n = control->count;
+        par_Reader[i].pid = n;
+        control->procesos[n].pid = n;
+        control->procesos[n].estado = 1;
+        control->procesos[n].tipo = 0;
+        control->count = n + 1;
+
+        // signal semaforo control
+        sem_signal(semid_control);
+
+        if (shmdt(memoria) == -1)
+        {
+            perror("shmdt");
+            exit(1);
+        }
+
+        if (pthread_create(&hilos[i], NULL, preader, (void *)&par_Reader[i]) != 0)
+        {
+            perror("pthread_create");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // los hilos no me funcionaban cuando los mandaba con &, ejemplo &hilos[i]
+    for (int i = 0; i < NL; i++)
+    {
+        if (pthread_join(hilos[i], NULL) != 0)
+        {
+            perror("pthread_join");
+            exit(EXIT_FAILURE);
+        }
+    }
+
     return 0;
 }
 
+/************************************************************************************************
+//***********************************************************************************************
 
-/**
-**
-**/
-
-// semget(key_t key, int nsems, int semflg);
-// semop(int semid, struct sembuf *sops, size_t nsops);
 
 void reader(int lector_id, int tiempo_lectura, int tiempo_dormir)
 {
+	key_t key = ftok("memoria_compartida", 'R'); // usar la misma clave que en el otro programa
+    if (key == -1)
+    {
+        perror("ftok");
+        exit(1);
+    }
+    int shm_id = shmget(key, 0, 0666);
     // obtener el identificador de la memoria compartida
-    int shm_id = shmget(SHM_KEY, SIZE_LINEA, 0666); // SIZE_LINEA -> era SHARED_MEMORY_SIZE
     if (shm_id == -1)
     {
         perror("shmget");
@@ -131,3 +456,5 @@ void reader(int lector_id, int tiempo_lectura, int tiempo_dormir)
     // detach (aqui o en main?)
     shmdt(shm_addr);
 }
+
+*/
